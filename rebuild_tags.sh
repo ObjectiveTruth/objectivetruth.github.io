1b16bf3b608f571e63879d3ac24215091543bdae
ObjectiveTruth <j.mendez@rogers.com>
2015-07-08 22:27:51 -0400

    Updated rebuild script to not require vimscript

--- rebuild_tags.sh
+++ rebuild_tags.sh
@@ -1,20 +1,23 @@
 #! /bin/bash
 #Finds all unique tags, and creates a directory structure for them under 
 #<root>/tags/<tagname> with a list of the posts with that tag
+echo "Creating vimscript"
+echo -e ":set hidden\n:set silent\n:first\nqq\ngg\n/tags:\nj\nV\n/---\n:w >> ../alltags.txt\ngg\nq\n:silent argdo normal @q\n:qall" > create_list_of_tags.vim &&
 
-echo "Creating list of tags using vim.."
+echo "Creating list of tags using vim.." &&
 touch alltags.txt &&
-vim -s "non_website_resources/create_list_of_tags.vim" ./_posts/*.md &&
+vim -s "create_list_of_tags.vim" ./_posts/*.md &&
+rm create_list_of_tags.vim &&
 
-echo "Reducing to unique entries only"
+echo "Reducing to unique entries only" &&
 sort alltags.txt | uniq -u > uniquetags.txt &&
 rm alltags.txt &&
 
-echo "Removing prepeding '- ' "
+echo "Removing prepeding '- ' " &&
 sed -i "s/- //g" uniquetags.txt &&
 
 #Slugify all lines
-echo "Slugifying all entries"
+echo "Slugifying all entries" &&
 cat uniquetags.txt | sed -e 's/[^[:alnum:]]/-/g' | tr -s '-' | tr A-Z a-z > slugs.txt &&
 rm uniquetags.txt &&
 
