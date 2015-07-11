0d51bda468f17ba33c3ac113b143132bd9376b8f
ObjectiveTruth <j.mendez@rogers.com>
2015-07-11 01:54:49 -0400

    Very efficient rebuid tags script using sed

--- rebuild_tags.sh
+++ rebuild_tags.sh
@@ -1,20 +1,19 @@
 #! /bin/bash
 #Finds all unique tags, and creates a directory structure for them under 
 #<root>/tags/<tagname> with a list of the posts with that tag
-echo "Creating vimscript"
-echo -e ":set hidden\n:set silent\n:first\nqq\ngg\n/tags:\nj\nV\n/---\n:w >> ../alltags.txt\ngg\nq\n:silent argdo normal @q\n:qall" > create_list_of_tags.vim &&
-
-echo "Creating list of tags using vim.." &&
-touch alltags.txt &&
-vim -s "create_list_of_tags.vim" ./_posts/*.md &&
-rm create_list_of_tags.vim &&
-
+echo "Creating list of tags using sed" &&
+sed -n '/^tags:/,/---/p' _posts/*.md > alltags.txt &&
 echo "Reducing to unique entries only" &&
 sort alltags.txt | uniq -u > uniquetags.txt &&
+sort alltags.txt | uniq -d >> uniquetags.txt &&
 rm alltags.txt &&
 
 echo "Removing prepeding '- ' " &&
 sed -i "s/- //g" uniquetags.txt &&
+
+echo "Removing lines with tag: and --- in them" &&
+sed -i "/tags:/d" uniquetags.txt &&
+sed -i "/---/d" uniquetags.txt &&
 
 #Slugify all lines
 echo "Slugifying all entries" &&
@@ -23,7 +22,7 @@ cat uniquetags.txt | sed -e 's/[^[:alnum:]]/-/g' | tr -s '-' | tr A-Z a-z > slug
 echo "Removing old tags directory" &&
 rm -rf tags &&
 
-echo "Creating Directories"
+echo "Creating Directories" &&
 while read -u 3 -r tags_line && read -u 4 -r slugified_line; do
     mkdir -p "tags/$slugified_line" &&
 
@@ -33,7 +32,8 @@ while read -u 3 -r tags_line && read -u 4 -r slugified_line; do
     echo "---" >> tags/$slugified_line/index.html
 done 3<uniquetags.txt 4<slugs.txt
 
-rm slugs.txt &&
 rm uniquetags.txt &&
+rm slugs.txt &&
+
 
 echo "Completed!";
